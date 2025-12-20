ff21eb9727447c40749d21f7314f733107c36106
aa-ray-man <138124037+aa-ray-man@users.noreply.github.com>
2025-12-20 23:53:38 +0530

    Time: 2 ms (99.10%) | Memory: 47.2 MB (21.16%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,14 +1,18 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+            int complement = target - nums[i];  // Find the complement of the current number
+            
+            // Check if the complement exists in the HashMap
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
             }
-        }
-        return new int[]{};
+            
+            // Otherwise, add the current number and its index to the HashMap
+            map.put(nums[i], i);
+        } 
+        return new int[] {}; // If no solution is found, return an empty array
     }
 }
